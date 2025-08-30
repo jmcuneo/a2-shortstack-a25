1da4db75e107568e8b75ba78d1f7b31e0fa608f9
lwaaron2023 <alwaller@wpi.edu>
2025-08-30 02:33:52 -0400

    Added functionality such that the form now sends data to the server

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,38 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
 const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+    // stop form submission from trying to load
+    // a new .html page for displaying results...
+    // this was the original browser behavior and still
+    // remains to this day
+    event.preventDefault()
+    //retrieves the elements of the form
+    const firstName = document.getElementById( "firstName" )
+    const lastName = document.getElementById( "lastName" )
+    const address = document.getElementById( "address" )
+    const shirts = document.getElementById( "shirts" )
+    const jackets = document.getElementById( "jackets" )
+    const hats = document.getElementById( "hats" )
 
-  const response = await fetch( "/submit", {
-    method:"POST",
-    body 
-  })
+    if(firstName && lastName && address && shirts && jackets && hats){
+        //creates a json with the information contained within the form
+        const json = {firstName: firstName.value, lastName: lastName.value, address: address.value, shirts:shirts.value, jackets:jackets.value, hats:hats.value};
+        const body = JSON.stringify(json);
+        //sends a post request to the server
+        const response = await fetch( "/submit", {
+            method:"POST",
+            body
+        })
 
-  const text = await response.text()
+        const text = await response.text()
 
-  console.log( "text:", text )
+        console.log( "text:", text )
+    }
+
+    else{
+        console.log("Error: failed to fetch form elements")
+    }
+
 }
 
 window.onload = function() {
